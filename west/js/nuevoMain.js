16d7eb48f32749ccc2aefff71d96683f3bcc5e69
majixux <109925189+majixux@users.noreply.github.com>
2023-05-22 12:11:11 -0600

    22/5
    Dejo mi ultimo intento, estuve cambiando la media asi que aun me falta hacerlo responsive pero le mando al de hombre y mujer. aun me falta lo de vue para el carrito ecommerce

---
+++ west/js/nuevoMain.js
@@ -0,0 +1,31 @@
+
+
+
+let listElements = document.querySelectorAll('.list__button--click');
+
+
+listElements.forEach(listElement => {
+    listElement.addEventListener('click', ()=> {
+        
+        listElement.classList.toggle('arrow');
+        
+
+        let height = 0;
+        let menu = listElement.nextElementSibling;
+        if(menu.clientHeight == "0"){
+            height=menu.scrollHeight;
+        }
+
+        menu.style.height = `${height}px`;
+    })
+});
+
+const $openClose = document.getElementById("open__close"),
+      $aside = document.getElementById("aside");
+
+$openClose.addEventListener("click", ()=>{
+    $aside.classList.toggle("desplegar")
+})
+
+
+
